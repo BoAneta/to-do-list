f5fc31bc86efe558d9e8db77f6402c640c92ac01
Aneta Borowski <borowski.aneta@gmail.com>
2022-11-24 15:02:46 +0100

    tasks function added

--- JS/script.js
+++ JS/script.js
@@ -3,11 +3,38 @@
    const buttonElement = document.querySelector(".js-button");
    const taskListElement = document.querySelector(".js-taskList");
 
+   const tasks = [
+    {
+        content: "zjeść obiad",
+        done: false,
+    },
+    {
+        content: "umyć okna",
+        done: true,
+    },
+   ];
+
+   const render = () => {
+    let htmlString = "";
+
+    for (const task of tasks) {
+        htmlString += `
+        <li>
+        ${task.content}
+        </li>
+        `;
+    }
+    document.querySelector(".js-taskList").innerHTML = htmlString;
+   }
+
     const onFormSubmit = (event) => {
     event.preventDefault();
     };
     
    const init = () => {
+
+    render();
+
     const formElement = document.querySelector(".js-form");
 
     formElement.addEventListener("submit", onFormSubmit);
